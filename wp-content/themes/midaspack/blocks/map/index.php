f6ceba991b61abd4ac9bc9e27cc12ded0a1e709f
misha <misha@ninesquares.studio>
2024-03-22 16:59:37 +0200

    updated map

--- wp-content/themes/midaspack/blocks/map/index.php
+++ wp-content/themes/midaspack/blocks/map/index.php
@@ -1,5 +1,5 @@
 <section class="mp-map mt-[30px] desktopMin:mt-[60px] max-w-[1440px] m-auto">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2526.177660630726!2d25.287475999999998!3d50.7166431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4725997326a511f3%3A0xe7c9940af602c464!2z0LLRg9C70LjRhtGPINCb0YzQstGW0LLRgdGM0LrQsCwgMTU0LCDQm9GD0YbRjNC6LCDQktC-0LvQuNC90YHRjNC60LAg0L7QsdC70LDRgdGC0YwsIDQzMDAw!5e0!3m2!1suk!2sua!4v1710746474749!5m2!1suk!2sua"
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1263.0704262300226!2d25.286442!3d50.717326!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472597cfbb56e739%3A0x5c43b53e2c424e78!2z0KLQntCSINCc0ZbQtNCw0YEg0J_QsNC6LCDQntCe0J4g0JzQuNC00LDRgSDQn9Cw0LosIE1pZGFzUGFjayBMTEM!5e0!3m2!1suk!2sua!4v1711119532596!5m2!1suk!2sua"
             style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
             class="block w-full h-[235px] desktopMin:h-[475px]"></iframe>
 </section>
